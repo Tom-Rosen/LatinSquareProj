6d9298cadf949f12ff2999fa150affa540dea6b1
Tom Rosen <107373056+Tom-Rosen@users.noreply.github.com>
2022-06-19 17:38:11 -0500

    Update latin_square_functions-4.c

--- latin_square_functions-4.c
+++ latin_square_functions-4.c
@@ -4,7 +4,6 @@
  * trrosen
  * Tom Rosen
  * 908 190 4204
- * OTHER COMMENTS FOR THE GRADER (OPTIONAL)
  *
  * @creator: Tom Rosen (trrosen@wisc.edu)
  * @modified: 11/13/21
@@ -16,47 +15,15 @@
 #include <string.h>
 #include "latin_square_functions.h"
 
-// Set this to 1 to enable dbgprintf statements, make sure to set it back to 0 
-// before submitting!
 #define DEBUG               0 
 #define dbgprintf(...)      if (DEBUG) { printf(__VA_ARGS__); }
 #define dbgprint_latin_square(n, LS) if (DEBUG) { Print_Latin_Square(n, LS); }
 
-/******************************************************************************
- * Data Types and Structures
- *****************************************************************************/
-
-// ADD ANY ADDITIONAL DATA TYPES OR STRUCTSvHERE 
-
-/******************************************************************************
- * Globals
- *****************************************************************************/
 extern void Print_Latin_Square(const size_t n, char **latin_square);
-
-/******************************************************************************
- * Helper functions
- *****************************************************************************/
-
-// ADD ANY HELPER FUNCTIONS YOU MIGHT WRITE HERE 
-
-/******************************************************************************
- * Verificaiton functions
- *****************************************************************************/
 
 /* 
  * This function takes the name of the file containing the latin square
  * and reads in the data to the the latin_square parameter.  
- *
- * There are many approaches that will work to read in the latin square data.
- * In any approach you choose, you will need to do at least the following:
- *     1) open the file 
- *     2) read in the text from the file
- *     3) figure out the dimensions of the latin square (n)
- *     4) reserve memory for the latin_square. This requires 2 steps
- *         4a) reserve an array of pointers to the rows
- *         4b) reserve an array of characters for each row
- *     5) fill in the latin_square data structure 
- *     6) close the file
  *
  * @param filename The name of the file to read in
  * @param latin_square_in A pointer to the latin square variable in main
@@ -178,12 +145,6 @@ int Verify_Alphabet(const size_t n, char **latin_square) {
  * duplicate symbols. This means that you will have at most n row error prints
  * and n column error prints.
  * 
- * Note: Test all rows first then test all columns.
- * 
- * Error messages have been included for you. Do not change the format out the 
- * print statements, as our grading scripts will use exact string matching. You
- * change the variable passed as the formatting argument to printf.
- * 
  * @param n The value of both dimensions of the latin square (i.e. nxn)
  * @param latin_square_in A pointer to the latin square variable in main
  * @return 1 if valid, 0 if not
@@ -264,8 +225,6 @@ int Verify_Alphabet(const size_t n, char **latin_square) {
 /* 
  * This function calls free to allow all memory used by the latin_square 
  * verification program to be reclaimed.
- *
- * Note: you will have n+1 calls to free
  *
  * @param n The value of both dimensions of the latin square (i.e. nxn)
  * @param latin_square_in A pointer to the latin square variable in main
